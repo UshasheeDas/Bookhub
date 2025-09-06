18f6bdfe9d567c4105b992b1b6b66e63a69cefc7
Ushashee Das <ushashee44@gmail.com>
2025-09-06 22:57:54 +0530

    Updated frontend

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BookhubLanding from "./pages/landing/landing";
+
+function App() {
+  return (
+    <div>
+      <BookhubLanding />
+    </div>
+  );
+}
+
+export default App;
